8b58c3758808b1a29da0263bd11a871108ecee37
Tiago Segatti <tiago01segatti@gmail.com>
2024-05-24 17:11:57 -0300

    Removendo linhas

--- Unidade 6/Uni6Exe1.java
+++ Unidade 6/Uni6Exe1.java
@@ -8,23 +8,19 @@ public class Uni6Exe1 {
        this.exibirVetor(numeros);
         scan.close();
     }
-/////// Aqui pode colocar 
     public void lerValores(Scanner scan,int[] numeros){
         for (int i = 0; i < numeros.length; i++) {
             System.out.println("Informe um valor para guardar no index " + i);
             numeros[i]=scan.nextInt();
         }
     }
-
     public void exibirVetor(int [] vetor){
         for(int i= vetor.length-1;i>=0;i--){
             System.out.println("index: "+i+" valor guardado: "+vetor[i]);
         }
     }
-
     public static void main(String[] args) {
         new Uni6Exe1();
     }
 
-    /////////
 }
