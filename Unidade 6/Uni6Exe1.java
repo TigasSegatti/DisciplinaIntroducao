9d887bee1468c2d99bb335fbdd7306222be0460b
Tiago Segatti <tiago01segatti@gmail.com>
2024-05-24 14:33:40 -0300

    Adicionando métodos

--- Unidade 6/Uni6Exe1.java
+++ Unidade 6/Uni6Exe1.java
@@ -4,17 +4,27 @@ public class Uni6Exe1 {
     public Uni6Exe1() {
         Scanner scan = new Scanner(System.in);
         int numeros[] = new int[10];
+        this.lerValores(scan, numeros);
+       this.exibirVetor(numeros);
+        scan.close();
+    }
+/////// Aqui pode colocar 
+    public void lerValores(Scanner scan,int[] numeros){
         for (int i = 0; i < numeros.length; i++) {
             System.out.println("Informe um valor para guardar no index " + i);
             numeros[i]=scan.nextInt();
         }
-        for(int j= numeros.length;j>=0;j--){
-            System.out.println("index: "+j+" valor guardado: "+numeros[j]);
+    }
+
+    public void exibirVetor(int [] vetor){
+        for(int i= vetor.length-1;i>=0;i--){
+            System.out.println("index: "+i+" valor guardado: "+vetor[i]);
         }
-        scan.close();
     }
 
     public static void main(String[] args) {
         new Uni6Exe1();
     }
+
+    /////////
 }
