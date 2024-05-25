25af30fb30d126d968ba00066521a3e09e2931d5
Tiago Segatti <tiago01segatti@gmail.com>
2024-05-25 19:14:00 -0300

    Adicionando método de leitura

--- Unidade 6/Uni6Exe9.java
+++ Unidade 6/Uni6Exe9.java
@@ -3,13 +3,24 @@ import java.util.Scanner;
 public class Uni6Exe9 {
     public Uni6Exe9() {
     Scanner scan= new Scanner(System.in);
-    quantidade=0;
-    int mulheres= new int[quantidade];
-
-    
+    int[] sexos = new int[30];
+    int[] notas = new int[30];
+    int[] idades = new int[30];
+    this.lerDados(scan, sexos, notas, idades);
 
 
     scan.close();
+    }
+    public void lerDados(Scanner scan,int sexos[], int notas[],int idades[]){
+        for (int i = 0; i < 30; i++) {
+            System.out.println("Digite o sexo do cliente (1=feminino, 2=masculino):");
+            sexos[i] = scan.nextInt();
+            System.out.println("Digite a nota do cliente (0 a 10):");
+            notas[i] = scan.nextInt();
+            System.out.println("Digite a idade do cliente:");
+            idades[i] = scan.nextInt();
+        }
+
     }
 
     public static void main(String[] args) {
